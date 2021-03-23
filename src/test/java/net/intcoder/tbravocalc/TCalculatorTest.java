8be290443478e5234fcacd89ed36cccdd7df249b
Nikita Demyanok <nikita.demyanok15@gmail.com>
2021-03-23 14:31:17 +0300

    Refactoring

--- src/test/java/net/intcoder/tbravocalc/TCalculatorTest.java
+++ src/test/java/net/intcoder/tbravocalc/TCalculatorTest.java
@@ -7,11 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TCalculatorTest {
     @Test
-    @Disabled
     void main() throws Exception {
-        TCalculator.main("-s", "/tmp/tmp.uW5uj05V0j/spreadsheet.txt",
-                "-t", "3101",
-                "-d", "17",
-                "-r");;
+        //
     }
 }
