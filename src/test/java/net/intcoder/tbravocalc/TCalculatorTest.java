592b8b416bf4728ef3c79d6ebc436490d7c97e3a
Nikita Demyanok <nikita.demyanok15@gmail.com>
2021-03-27 16:58:55 +0300

    TCalculatorTest refactoring

--- src/test/java/net/intcoder/tbravocalc/TCalculatorTest.java
+++ src/test/java/net/intcoder/tbravocalc/TCalculatorTest.java
@@ -8,15 +8,16 @@ class TCalculatorTest {
 
     @Test
     void main() throws Exception {
-        var tmp = Files.createTempFile("test-spreadsheet.txt", "tmp");
+        var tmp = Files.createTempFile("test-spreadsheet.txt.", ".tmp");
 
         try (var in = TCalculatorTest.class.getResourceAsStream("/spreadsheet.txt");
              var out = Files.newOutputStream(tmp)) {
             in.transferTo(out);
         }
 
-        TCalculator.main("-s", tmp.toAbsolutePath().toString(),
-                                "-t4524");
+        TCalculator.main(
+                "-s", tmp.toAbsolutePath().toString(),
+                "-t4524");
 
         Files.delete(tmp);
     }
